371fe8151e5e6148eab3c3079d12294d6c6d7437
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2021-05-19 19:02:24 +0900

    エディター画面の見た目を作成

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,19 @@
 import * as React from 'react'
 import { render } from 'react-dom'
+import styled, { createGlobalStyle } from 'styled-components'
+import { Editor } from './pages/editor'
+
+const GlobalStyle = createGlobalStyle`
+  body * {
+    box-sizing: border-box;
+  }
+`
 
 const Main = (
-  <h1>Markdown Editers</h1>
+  <>
+    <GlobalStyle />
+    <Editor />
+  </>
 )
 
 render(Main, document.getElementById('app'))
